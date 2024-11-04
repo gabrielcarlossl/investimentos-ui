af9deb4d377a63059201e79fbfd3577de44c0c42
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-11-04 17:53:10 -0300

    added sections on main page

---
+++ src/pages/Main.jsx
@@ -0,0 +1,15 @@
+import AboutUs from '../components/AboutUs'
+import FindInvestors from '../components/FindInvestors'
+import HowItWorks from '../components/HowItWorks'
+
+const Main = () => {
+    return (
+        <>
+            <FindInvestors />
+            <HowItWorks />
+            <AboutUs/>
+        </>
+    )
+}
+
+export default Main
