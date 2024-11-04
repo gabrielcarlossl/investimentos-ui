cafd64aa7215290a70431a95950a25df005934c7
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-11-04 17:54:55 -0300

    added styles and vertical line component

---
+++ src/components/VerticalLines.jsx
@@ -0,0 +1,20 @@
+
+import { styled } from '@mui/system';
+
+export const EndSectionVerticalLine = styled('div')({
+    backgroundImage: 'linear-gradient(180deg, #011826 0, #45718C)',
+    height: '70px',
+    width: '2px',
+});
+export const StartSectionVerticalLine = styled('div')({
+    backgroundImage: 'linear-gradient(180deg, #45718C 0, #F2F2F0)',
+    height: '70px',
+    width: '2px',
+});
+export const StartSectionVerticalLineLight = styled('div')({
+    backgroundImage: 'linear-gradient(180deg, #45718C 0, #87adc5)',
+    height: '70px',
+    width: '2px',
+});
+
+
