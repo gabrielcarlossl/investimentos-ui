0df5620f8460c816e43f5898197a2fa92d4701d3
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-11-11 09:07:22 -0300

    refactor: page names

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AppBarMenu from '../components/AppBar';
-import Main from '../pages/Main';
-import Footer from '../components/Footer';
-import Formulario from '../pages/Form'
+// Pages
+import Main from '../pages/Main'
 import NotFound from '../pages/NotFound'
-import FormSuccess from '../pages/FormSuccess';
+import FormSuccess from '../pages/FormSuccess'
+import JoinUs from '../pages/JoinUs'
+
+// Template Layout
+import AppBarMenu from '../components/AppBar'
+import Footer from '../components/Footer'
+
+// Utils
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const Root = () => {
   return (
@@ -12,7 +17,7 @@ const Root = () => {
     <AppBarMenu />
      <Routes>
        <Route path='/' element={<Main/>} />
-       <Route path='/formulario' element={<Formulario />} />
+       <Route path='/formulario' element={<JoinUs />} />
        <Route path='/success' element={<FormSuccess />} />
        <Route path='*' element={<NotFound />} />
      </Routes>
