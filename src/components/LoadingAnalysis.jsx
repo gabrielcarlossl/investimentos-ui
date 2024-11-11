c23737cd79a2ab446db70459627073e595754409
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-11-11 14:50:55 -0300

    added loading text translation

---
+++ src/components/LoadingAnalysis.jsx
@@ -0,0 +1,32 @@
+// Components
+import { Box, CircularProgress } from '@mui/material'
+import { EndSectionVerticalLine } from './VerticalLines'
+
+// Styles
+import styles from './styles/loadingAnalysis.module.scss'
+
+// Translation
+import { useTranslation } from 'react-i18next'
+
+const LoadingAnalysis = () => {
+  const { t } = useTranslation()
+
+  return (
+    <Box
+      sx={{
+        '.MuiCircularProgress-root': {
+          color: '#45718C'
+        }
+      }}
+      className={styles.loading_container}
+    >
+      <div>
+        <h1>{t('loadingText')}</h1>
+        <CircularProgress color='secondary' />
+      </div>
+      <EndSectionVerticalLine />
+    </Box>
+  )
+}
+
+export default LoadingAnalysis
