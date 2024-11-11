eecaeeba7b2676bac1e6face740a66c43fd3c065
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-11-11 14:52:42 -0300

    refactor: added  join us page condition to display component if it is loading or with error or has user's profile data

--- src/pages/JoinUs.jsx
+++ src/pages/JoinUs.jsx
@@ -1,11 +1,21 @@
-import Form from '../components/Form'
+// Components
+import SuccessAnalysis from '../components/SuccessAnalysis'
+import LoadingAnalysis from '../components/LoadingAnalysis'
+import RejectedAnalysis from '../components/RejectedAnalysis'
+import ProfileAnalysis from '../components/ProfileAnalysis'
+
+// Redux
+import { useSelector } from 'react-redux'
 
 const JoinUs = () => {
-  return (
-    <div>
-      <Form/>
-    </div>
-  )
+  const profile = useSelector((state) => state.ProfileReducer);
+  const { item, isError, isLoading } = profile;
+
+  if (isLoading) return <LoadingAnalysis />;
+  if (isError) return <RejectedAnalysis />;
+  if (!item?.id) return <ProfileAnalysis />;
+  
+  return <SuccessAnalysis />
 }
 
-export default JoinUs
+export default JoinUs;
