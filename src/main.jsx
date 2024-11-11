329cdf49535ff4fada6fba8e7752ed14a36a4582
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-11-11 11:29:57 -0300

    removed unnecessary comma

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
   </React.StrictMode>,
 )
